f78c13221934c58839f6d25e6ce98e6c46f06b9f
cbommas <chabom@amazon.com>
2018-05-29 15:12:49 -0700

    Re-organized voice types and added missing voices. Ensured that the UI waits for the voice selection UIPickerView to be fully loaded.

--- Polly-Sample/Swift/PollySample/ViewController.swift
+++ Polly-Sample/Swift/PollySample/ViewController.swift
@@ -24,76 +24,107 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
 	@IBOutlet weak var stackView: UIStackView!
 
 	let sampleTexts: [AWSPollyVoiceId: String] = [
-		AWSPollyVoiceId.gwyneth: "Helo 'na! Gwyneth ydw i. Teipiwch unrhywbeth yma a fe wna i ei ddarllen.",
-
+		//Danish
 		AWSPollyVoiceId.mads: "Hej! Mit navn er Mads. Jeg kan oplæse enhver tekst, som du skriver her.",
 		AWSPollyVoiceId.naja: "Hej! Mit navn er Naja. Jeg kan oplæse enhver tekst, som du skriver her.",
 
-		AWSPollyVoiceId.hans: "Hallo, mein Name ist Hans. Ich werde jeden Text vorlesen, den Sie eingeben.",
+        //Dutch
+        AWSPollyVoiceId.lotte: "Hoi! mijn naam is Lotte. Ik lees elke tekst voor die je hier invoert.",
+        AWSPollyVoiceId.ruben: "Hoi! mijn naam is Ruben. Ik lees elke tekst voor die je hier invoert.",
+
+        //English (Australian)
+        AWSPollyVoiceId.nicole: "Hi there, my name is Nicole. I will read any text you type here.",
+        AWSPollyVoiceId.russell: "Hi there, my name is Russel. I will read any text you type here.",
+        
+        //English (British)
+        AWSPollyVoiceId.amy: "Hi! My name is Amy. I will read any text you type here.",
+        AWSPollyVoiceId.brian: "Hi! My name is Brian. I will read any text you type here.",
+        AWSPollyVoiceId.emma: "Hi! My name is Emma. I will read any text you type here.",
+
+        //English (Indian)
+        AWSPollyVoiceId.raveena: "Hi! My name is Raveena. I can read any text you type here.",
+        AWSPollyVoiceId.aditi: "Hi! My name is Aditi. I can read any text you type here.",
+        
+        //English (US)
+        AWSPollyVoiceId.ivy: "Hi! My name is Ivy. I will read any text you type here.",
+        AWSPollyVoiceId.joanna: "Hi! My name is Joanna. I will read any text you type here.",
+        AWSPollyVoiceId.joey: "Hi! My name is Joey. I will read any text you type here.",
+        AWSPollyVoiceId.justin: "Hi! My name is Justin. I will read any text you type here.",
+        AWSPollyVoiceId.kendra: "Hi! My name is Kendra. I will read any text you type here.",
+        AWSPollyVoiceId.kimberly: "Hi! My name is Kimberly. I will read any text you type here.",
+        AWSPollyVoiceId.matthew: "Hi! My name is Matthew. I will read any text you type here.",
+        AWSPollyVoiceId.salli: "Hi! My name is Salli. I will read any text you type here.",
+
+        //English (Welsh)
+        AWSPollyVoiceId.geraint: "Hi! I'm Geraint. I can read any text you type here",
+
+        //French
+        AWSPollyVoiceId.celine: "Salut, je m'appelle Céline. Je vais lire le texte que vous écrirez ici.",
+        AWSPollyVoiceId.mathieu: "Salut, je m'appelle Mathieu. Je vais lire le texte que vous écrirez ici.",
+
+        //French (Canadian)
+        AWSPollyVoiceId.chantal: "Salut, je m'appelle Chantal. Je vais lire le texte que vous écrirez ici.",
+
+        //German
+        AWSPollyVoiceId.hans: "Hallo, mein Name ist Hans. Ich werde jeden Text vorlesen, den Sie eingeben.",
 		AWSPollyVoiceId.marlene: "Hallo, mein Name ist Marlene. Ich werde jeden Text vorlesen, den Sie eingeben.",
+        AWSPollyVoiceId.vicki: "Hallo, mein Name ist Vicki. Ich werde jeden Text vorlesen, den Sie eingeben.",
 
-		AWSPollyVoiceId.nicole: "Hi there, my name is Nicole. I will read any text you type here.",
-		AWSPollyVoiceId.russell: "Hi there, my name is Russel. I will read any text you type here.",
+		//Icelandic
+        AWSPollyVoiceId.dora: "Ég heiti Dóra. Ég les upphátt allan texta sem þú skrifar hér.",
+        AWSPollyVoiceId.karl: "Ég heiti Karl. Ég les upphátt allan texta sem þú skrifar hér.",
 
-		AWSPollyVoiceId.amy: "Hi! My name is Amy. I will read any text you type here.",
-		AWSPollyVoiceId.brian: "Hi! My name is Brian. I will read any text you type here.",
-		AWSPollyVoiceId.emma: "Hi! My name is Emma. I will read any text you type here.",
+        //Italian
+        AWSPollyVoiceId.carla: "Ciao, mi chiamo Carla. Leggerò qualsiasi testo che digiterai qui.",
+        AWSPollyVoiceId.giorgio: "Ciao, mi chiamo Giorgio. Leggerò qualsiasi testo che digiterai qui.",
 
-		AWSPollyVoiceId.geraint: "Hi! I'm Geraint. I can read any text you type here",
+        //Japanese
+        AWSPollyVoiceId.mizuki: "はじめまして。ミズキです。読みたいテキストを入力してください。",
+        AWSPollyVoiceId.takumi: "はじめまして。ミズキです。読みたいテキストを入力してください。",
+		
+		//Korean
+        AWSPollyVoiceId.seoyeon: "안녕하세요? 외국어를 구사하십니까? 이 세상에는 수많은 언어들이 있답니다。",
+        
+        //Norwegian
+        AWSPollyVoiceId.liv: "Hei! Jeg heter Liv. Skriv inn noe her, så leser jeg det opp.",
 
-		AWSPollyVoiceId.raveena: "Hi! My name is Raveena. I can read any text you type here.",
+        //Polish
+        AWSPollyVoiceId.ewa: "Cześć, mam na imię Ewa. Przeczytam każdy tekst, który tutaj wpiszesz.",
+        AWSPollyVoiceId.jacek: "Cześć, mam na imię Jacek. Przeczytam każdy tekst, który tutaj wpiszesz.",
+        AWSPollyVoiceId.jan: "Cześć, mam na imię Jan. Przeczytam każdy tekst, który tutaj wpiszesz.",
+        AWSPollyVoiceId.maja: "Cześć, mam na imię Maja. Przeczytam każdy tekst, który tutaj wpiszesz.",
 
-		AWSPollyVoiceId.ivy: "Hi! My name is Ivy. I will read any text you type here.",
-		AWSPollyVoiceId.joanna: "Hi! My name is Joanna. I will read any text you type here.",
-		AWSPollyVoiceId.joey: "Hi! My name is Joey. I will read any text you type here.",
-		AWSPollyVoiceId.justin: "Hi! My name is Justin. I will read any text you type here.",
-		AWSPollyVoiceId.kendra: "Hi! My name is Kendra. I will read any text you type here.",
-		AWSPollyVoiceId.kimberly: "Hi! My name is Kimberly. I will read any text you type here.",
-		AWSPollyVoiceId.salli: "Hi! My name is Salli. I will read any text you type here.",
+        //Portugese (Brazilian)
+        AWSPollyVoiceId.ricardo: "Olá, meu nome é Ricardo. Eu posso ler qualquer texto que você digitar aqui.",
+        AWSPollyVoiceId.vitoria: "Olá, meu nome é Vitória. Eu posso ler qualquer texto que você digitar aqui.",
 
-		AWSPollyVoiceId.conchita: "Hola! Mi nombre es Conchita. Puedo leer cualquier texto que introduzcas aquí.",
+        //Portugese (Europian)
+        AWSPollyVoiceId.cristiano: "Olá! O meu nome é Cristiano. Vou ler o texto que escrever aqui.",
+        AWSPollyVoiceId.ines: "Olá! O meu nome é Inês. Vou ler o texto que escrever aqui.",
+
+        //Romanian
+        AWSPollyVoiceId.carmen: "Bună, numele meu este Carmen. Pot citi orice text introdus aici.",
+
+        //Russian
+        AWSPollyVoiceId.maxim: "Привет! Меня зовут Максим. Я прочитаю любой текст который вы введете здесь.",
+        AWSPollyVoiceId.tatyana: "Привет! Меня зовут Татьяна. Я прочитаю любой текст который вы введете здесь.",
+
+        //Spanish (Castilian)
+        AWSPollyVoiceId.conchita: "Hola! Mi nombre es Conchita. Puedo leer cualquier texto que introduzcas aquí.",
 		AWSPollyVoiceId.enrique: "Hola! Mi nombre es Enrique. Puedo leer cualquier texto que introduzcas aquí.",
 
+        //Spanish (Latin American)
 		AWSPollyVoiceId.miguel: "Hola! Mi nombre es Miguel. Puedo leer cualquier texto que introduzcas aquí.",
 		AWSPollyVoiceId.penelope: "Hola! Mi nombre es Penelope. Puedo leer cualquier texto que introduzcas aquí.",
-
-		AWSPollyVoiceId.mizuki: "はじめまして。ミズキです。読みたいテキストを入力してください。",
-
-		AWSPollyVoiceId.chantal: "Salut, je m'appelle Chantal. Je vais lire le texte que vous écrirez ici.",
-
-		AWSPollyVoiceId.celine: "Salut, je m'appelle Céline. Je vais lire le texte que vous écrirez ici.",
-		AWSPollyVoiceId.mathieu: "Salut, je m'appelle Mathieu. Je vais lire le texte que vous écrirez ici.",
-
-		AWSPollyVoiceId.dora: "Ég heiti Dóra. Ég les upphátt allan texta sem þú skrifar hér.",
-		AWSPollyVoiceId.karl: "Ég heiti Karl. Ég les upphátt allan texta sem þú skrifar hér.",
-
-		AWSPollyVoiceId.carla: "Ciao, mi chiamo Carla. Leggerò qualsiasi testo che digiterai qui.",
-		AWSPollyVoiceId.giorgio: "Ciao, mi chiamo Giorgio. Leggerò qualsiasi testo che digiterai qui.",
-
-		AWSPollyVoiceId.liv: "Hei! Jeg heter Liv. Skriv inn noe her, så leser jeg det opp.",
-
-		AWSPollyVoiceId.lotte: "Hoi! mijn naam is Lotte. Ik lees elke tekst voor die je hier invoert.",
-		AWSPollyVoiceId.ruben: "Hoi! mijn naam is Ruben. Ik lees elke tekst voor die je hier invoert.",
-
-		AWSPollyVoiceId.ewa: "Cześć, mam na imię Ewa. Przeczytam każdy tekst, który tutaj wpiszesz.",
-		AWSPollyVoiceId.jacek: "Cześć, mam na imię Jacek. Przeczytam każdy tekst, który tutaj wpiszesz.",
-		AWSPollyVoiceId.jan: "Cześć, mam na imię Jan. Przeczytam każdy tekst, który tutaj wpiszesz.",
-		AWSPollyVoiceId.maja: "Cześć, mam na imię Maja. Przeczytam każdy tekst, który tutaj wpiszesz.",
-
-		AWSPollyVoiceId.ricardo: "Olá, meu nome é Ricardo. Eu posso ler qualquer texto que você digitar aqui.",
-		AWSPollyVoiceId.vitoria: "Olá, meu nome é Vitória. Eu posso ler qualquer texto que você digitar aqui.",
-
-		AWSPollyVoiceId.cristiano: "Olá! O meu nome é Cristiano. Vou ler o texto que escrever aqui.",
-		AWSPollyVoiceId.ines: "Olá! O meu nome é Inês. Vou ler o texto que escrever aqui.",
-
-		AWSPollyVoiceId.carmen: "Bună, numele meu este Carmen. Pot citi orice text introdus aici.",
-
-		AWSPollyVoiceId.maxim: "Привет! Меня зовут Максим. Я прочитаю любой текст который вы введете здесь.",
-		AWSPollyVoiceId.tatyana: "Привет! Меня зовут Татьяна. Я прочитаю любой текст который вы введете здесь.",
-
+        
+        //Swedish
 		AWSPollyVoiceId.astrid: "Hejsan! Jag heter Astrid och läser upp det som skrivs här.",
 
-		AWSPollyVoiceId.filiz: "Merhaba, benim adım Filiz. Buraya girdiğiniz her metni okuyabilirim."
+        //Turkish
+		AWSPollyVoiceId.filiz: "Merhaba, benim adım Filiz. Buraya girdiğiniz her metni okuyabilirim.",
+        
+        //Welsh
+        AWSPollyVoiceId.gwyneth: "Helo 'na! Gwyneth ydw i. Teipiwch unrhywbeth yma a fe wna i ei ddarllen."
 	]
 
 	var originalConstraint: CGFloat!
@@ -150,7 +181,7 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
 			}
 
 			return nil
-		}
+		}.waitUntilFinished()
 
 		// Handle keyboard changing frame
 		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: .UIKeyboardWillChangeFrame, object: nil)
