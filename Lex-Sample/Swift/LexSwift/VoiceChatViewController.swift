5a44e4c4fe7b5bc79ff23e49349d062ef38436ff
Abhash Kumar Singh <thisisabhash@gmail.com>
2021-04-27 22:21:11 -0700

    fix(Lex-Sample): Fix memory issue in VoiceChatViewController

--- Lex-Sample/Swift/LexSwift/VoiceChatViewController.swift
+++ Lex-Sample/Swift/LexSwift/VoiceChatViewController.swift
@@ -24,7 +24,6 @@ class VoiceChatViewController: UIViewController, AWSLexVoiceButtonDelegate {
     @IBOutlet weak var input: UILabel!
     @IBOutlet weak var output: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         (self.voiceButton as AWSLexVoiceButton).delegate = self
@@ -32,14 +31,14 @@ class VoiceChatViewController: UIViewController, AWSLexVoiceButtonDelegate {
     
     func voiceButton(_ button: AWSLexVoiceButton, on response: AWSLexVoiceButtonResponse) {
         DispatchQueue.main.async(execute: {
-            // `inputranscript` is the transcript of the voice input to the operation
+            // `inputTranscript` is the transcript of the voice input to the operation
             if let inputTranscript = response.inputTranscript {
                 print("Input Transcript: " + inputTranscript)
                 self.input.text = "\"\(inputTranscript)\""
             }
             
             if let outputText = response.outputText {
-                print("Input Transcript: " + outputText)
+                print("Output Transcript: " + outputText)
             }
 
             self.output.text = response.outputText
